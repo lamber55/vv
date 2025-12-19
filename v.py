5978f71075eb49323f1307fec33d644e8439b11e
lamber55 <48569060+lamber55@users.noreply.github.com>
2025-12-20 00:31:18 +0100

    Update shutdown message and command in v.py

--- v.py
+++ v.py
@@ -4,11 +4,9 @@ Created on Fri Dec 19 22:50:12 2025
 
 @author: mique
 """
-
 import os
 import time
 
-print("Virus activado... Reiniciando el sistema en 5 segundos...")
+print("¡Atención! Tu sistema se apagará en 5 segundos.")
 time.sleep(5)
-
-os.system("shutdown /r /t 0")
+os.system("shutdown /s /t 5")
